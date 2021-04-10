99ab35b818dacd45f5de00c84fa0cbdefcf7acab
Carrillo11 <renecarrillo120@gmail.com>
2021-04-09 19:45:18 -0600

    El ususario ya puede cerrar sesion

--- Desafio2/app/src/main/java/sv/edu/udb/desafio2/Perfil.java
+++ Desafio2/app/src/main/java/sv/edu/udb/desafio2/Perfil.java
@@ -7,9 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Perfil extends Activity {
-    private Button btnInicio, btnDetalle, btnUsuario;
+    private Button btnInicio, btnDetalle, btnUsuario, btnLogOut;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +48,21 @@ public class Perfil extends Activity {
                 finish();
             }
         });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Perfil.this, login.class));
+            }
+        });
     }
 
     private void initializeUI() {
         btnInicio = findViewById(R.id.btninicio);
         btnDetalle = findViewById(R.id.btndetalle);
         btnUsuario = findViewById(R.id.btnusuario);
+        btnLogOut = findViewById(R.id.btnsalir);
+
     }
 }
